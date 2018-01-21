d25408bb516a2de7a5181e98fab9c11e553b1645
zt19961212 <cxc4617010222>
2018-01-21 21:05:29 +0800

    上传效果图

--- flow/src/main/java/com/william/flow/data/user/UserCenter.java
+++ flow/src/main/java/com/william/flow/data/user/UserCenter.java
@@ -5,9 +5,6 @@ import com.william.flow.model.card.UserCard;
 
 /**
  * 用户中心的基本定义
- *
- * @author qiujuer Email:qiujuer@live.cn
- * @version 1.0.0
  */
 public interface UserCenter {
     // 分发处理一堆用户卡片的信息，并更新到数据库
